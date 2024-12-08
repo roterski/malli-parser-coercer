a05e609dbc20966baf306432ed74b773ab75827d
Piotr Roterski <pt.roterski@gmail.com>
2024-12-09 01:40:36 +0800

    add malli instrumentation and replace rcf with rct

--- src/com/example/core.clj
+++ src/com/example/core.clj
@@ -1,12 +1,14 @@
 (ns com.example.core
   (:require [malli.core :as ma]
             [muotti.core :as muotti]
-            [muotti.malli :as mm]
-            [hyperfiddle.rcf :as rcf]))
+            [muotti.malli :as mm]))
 
 (def malli-transformer (mm/transformer (muotti/->transformer mm/malli-config)))
 
 (defn parse&coerce
+  {:malli/schema [:=>
+                  [:cat [:* :any]]
+                  [:map-of :keyword :string]]}
   [& args]
   (->> args
        (ma/parse [:* [:cat :any :any]])
@@ -15,10 +17,19 @@
                         malli-transformer))
        (into {})))
 
-(rcf/enable!)
+^:rct/test
+(comment
+  (parse&coerce :jar-name 'user.jar)
+  ;;=> {:jar-name "user.jar"}
+  (parse&coerce :jar-name "user.jar" :version 15)
+  ;;=> {:jar-name "user.jar", :version "15"}
+  (parse&coerce "jar-name" 'user.jar :version '15)
+  ;;=> {:jar-name "user.jar", :version "15"}
+  (parse&coerce 'jar-name 'user.jar :version "15")
+  ;;=> {:jar-name "user.jar", :version "15"}
+  )
 
-(rcf/tests
- (parse&coerce :jar-name 'user.jar)               := {:jar-name "user.jar"}
- (parse&coerce :jar-name "user.jar" :version 15)  := {:jar-name "user.jar", :version "15"}
- (parse&coerce "jar-name" 'user.jar :version '15) := {:jar-name "user.jar", :version "15"}
- (parse&coerce 'jar-name 'user.jar :version "15") := {:jar-name "user.jar", :version "15"})
+;; left those below so instrumentation and tests are run on ns reload
+;; but usually I run them manually via calva.customREPLCommandSnippets
+((requiring-resolve 'malli.dev/start!))
+((requiring-resolve 'com.mjdowney.rich-comment-tests/run-ns-tests!) *ns*)
